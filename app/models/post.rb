a9393657242f51e3d588542e3d78aa48b6f7223e
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-09-21 19:44:48 -0400

    fixing author emails on posts index

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   def author
-    User.select(:email).where(id: self.user_id).collect(&:email)
+    email = User.select(:email).where(id: self.user_id).collect(&:email)
+    email.join("") # why do I have to do this???
   end
 end
